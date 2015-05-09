3b5a1bfabfe3fb1a8725ca24a9a69dccf7eb5d6f
lkoniecki <lukasz.koniecki@gmail.com>
2015-05-09 15:08:49 +0200

    Added UnsortedArray

---
+++ src/main/java/structures/UnsortedArray.java
@@ -0,0 +1,156 @@
+package structures;
+
+import java.util.Arrays;
+
+/**
+ * Weak typing unsorted array.<br/>
+ * Implmementation properties:<br/>
+ * <ul>
+ * <li>elemets order is not preserved during elements deletion</li>
+ * <li>if newly added element doesn't fit into currently allocated memory, size of the array is doubled</li>
+ * <li>after element is removed from the array and if there is less than 1/4 
+ * buckets occupied in the array, array is shrinked by half</li>
+ * </ul>
+ * @author Łukasz Koniecki
+ * @version 1.0.0
+ */
+public class UnsortedArray<V> {
+	//element not found in the array
+	public static final int ELEMENT_NOT_FOUND = -1;
+	//array elements
+	private Object[] elements;
+	//last element in the array pointer
+	private int pointer = 0;
+
+	/**
+	 * Creates array of type V and given size {@code size}
+	 * @param size size of the array
+	 * @throws IllegalArgumentException if given size is less than 1
+	 * @since 1.0.0
+	 */
+	public UnsortedArray(int size) {
+		if (size <= 0) {
+			throw new IllegalArgumentException("Array size must be grater than 0");
+		}
+		elements = new Object[size];
+	}
+
+	/**
+	 * Adds element to the end of the array
+	 * @param element element to add
+	 * @throws IllegalArgumentException if element is null
+	 * @since 1.0.0
+	 */
+	public void add(V element) {
+		if (element == null) {
+			throw new IllegalArgumentException("Element can not be null");
+		}
+
+		if (pointer < elements.length) {
+			elements[pointer++] = element;
+		} else {
+			elements = Arrays.copyOf(elements, elements.length * 2);
+			elements[pointer++] = element;
+		}
+	}
+
+	/**
+	 * Gets element at {@code index} in the array
+	 * @return element at possition {@code index}
+	 * @since 1.0.0
+	 */
+	public V get(int index) {
+		return (V) elements[index];
+	}
+
+	/**
+	 * Gets length of the array
+	 * @return length of the array
+	 * @since 1.0.0
+	 */
+	public int length() {
+		return pointer+1;
+	}
+
+	/**
+	 * Returns size of the array in the memory (in number of allocated elements)
+	 * @return size of the array (in number of allocated elements)
+	 * @since 1.0.0
+	 */
+	public int size() {
+		return elements.length;
+	}
+
+	/**
+	 * Returns position of the first element equal {@code element} in the array
+	 * @param element element to search
+	 * @return element position if the array if element was found in the array, -1 otherwise
+	 * @throw IllegalArgumentException if element is null
+	 * @since 1.0.0
+	 */
+	public int search(V element) {
+		if (element == null) {
+			throw new IllegalArgumentException("Element can not be null");
+		}
+
+		//if this is an empty array
+		if (pointer == 0) {
+			return ELEMENT_NOT_FOUND;
+		}
+
+		for (int idx = 0; idx < pointer; idx++) {
+			if (elements[idx] == element) {
+				return idx;
+			}
+		}
+
+		return ELEMENT_NOT_FOUND;
+	}
+
+	/**
+	 * Removes first element of value {@code element} from an array.
+	 * Array elements order may not be preserved.
+	 * @param element element to remove
+	 * @since 1.0.0
+	 */
+	public void delete(V element) {
+		//find first element of given value in the array
+		int idx = search(element);
+		//if element does not exist in the array
+		if (idx == ELEMENT_NOT_FOUND) {
+			//just return
+			return;
+		}
+
+		//if this is the last element in the array
+		if (pointer == 1) {
+			//just set it to null
+			elements[idx] = null;
+			//decrease pointer
+			pointer--;
+		} else {
+			//decrease pointer
+			pointer--;
+			//copy last elemnt from the array to a position of element being removed
+			elements[idx] = elements[pointer];
+			//set last element in the array to null
+			elements[pointer] = null;
+		}
+
+		//if less than 1/4 of available buckets in the array is available shrink the array
+		if (pointer < elements.length/4) {
+			//new array will be half size of the old array
+			elements = Arrays.copyOf(elements, elements.length / 2);
+		}
+	}
+
+	public String toString() {
+		StringBuffer b = new StringBuffer();
+		b.append("[");
+		for (int idx = 0; idx < pointer; idx++) {
+			b.append(elements[idx] + ",");
+		}
+		b.append("]");
+		return b.toString();
+	}
+}
---
+++ src/test/java/structures/UnsortedArrayTest.java
@@ -0,0 +1,85 @@
+package structures;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.util.Random;
+
+public class UnsortedArrayTest {
+	private static final Random RND = new Random();
+
+	@Test
+	public void testAddWithoutArrayEnlargement() {
+		int expectedArraySize = 10;
+		UnsortedArray<Integer> array = new UnsortedArray<Integer>(expectedArraySize);
+		fillArrayWithConsecutiveElements(array, 0, expectedArraySize);
+		assertEquals("Wrong array size", expectedArraySize, array.size());
+	}
+
+	@Test
+	public void testAddWithArrayEnlargement() {
+		int expectedArraySize = 10;
+		UnsortedArray<Integer> array = new UnsortedArray<Integer>(expectedArraySize);
+		fillArrayWithConsecutiveElements(array, 0, expectedArraySize);
+		assertEquals("Wrong array size", expectedArraySize, array.size());
+		fillArrayWithConsecutiveElements(array, 11, expectedArraySize);
+		assertEquals("Wrong array size", 2 * expectedArraySize, array.size());
+		fillArrayWithConsecutiveElements(array, 21, expectedArraySize);
+		assertEquals("Wrong array size", 4 * expectedArraySize, array.size());
+	}
+
+	@Test
+	public void testGet() {
+		int arraySize = 20;
+		UnsortedArray<Integer> array = new UnsortedArray<Integer>(arraySize);
+		fillArrayWithConsecutiveElements(array, 0, arraySize);
+
+		for (int idx = 0; idx < arraySize; idx++) {
+			assertEquals("Wrong element at index " + idx, idx, (int)array.get(idx));
+		}
+	}
+
+	@Test
+	public void testSearch() {
+		//fill array so that only 3/4 of the array is occupied
+		int arraySize = 15;
+		UnsortedArray<Integer> array = new UnsortedArray<Integer>(10);
+		fillArrayWithConsecutiveElements(array, 0, arraySize);
+
+		for (int idx = 0; idx < arraySize; idx++) {
+			assertEquals("Wrong element position", idx, array.search(idx));
+		}
+	}
+
+	@Test
+	public void testSearchElementNotFound() {
+		//fill array so that only 3/4 of the array is occupied
+		int arraySize = 15;
+		UnsortedArray<Integer> array = new UnsortedArray<Integer>(10);
+		fillArrayWithConsecutiveElements(array, 0, arraySize);
+
+		assertEquals("Element found", -1, array.search(1000));
+	}
+
+	@Test
+	public void testDelete() {
+		UnsortedArray<Integer> array = new UnsortedArray<Integer>(10);
+		//fill array so that only 3/4 of the array is occupied
+		fillArrayWithConsecutiveElements(array, 0, 15);
+		assertEquals("Wrong array size", 20, array.size());
+
+		for (int idx = 0; idx < 15; idx++) {
+			array.delete(idx);
+			assertEquals("Element not removed", -1, array.search(idx));
+		}
+
+		//array should be have been shinked once during the deletion
+		assertEquals("Wrong allocation size", 2, array.size());
+	}
+
+	private void fillArrayWithConsecutiveElements(UnsortedArray array, int first, int noElements) {
+		int last = first + noElements;
+		for (int idx = first; idx < last; idx++) {
+			array.add(idx);
+		}
+	}
+}
